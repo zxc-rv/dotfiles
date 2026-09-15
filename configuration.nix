93e150c05f11c26bd35a23ee887b65efc71c873a
zxc-rv <zxc.revolution@proton.me>
2026-09-16 00:18:26 +0300

    u

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@
     loader = {
       systemd-boot = {
         enable = true;
-        configurationLimit = 5;
+        configurationLimit = 10;
       };
       efi.canTouchEfiVariables = true;
     };
