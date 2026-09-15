0d77f346e0c88939126d625bb5216161ea864ed0
zxc-rv <zxc.revolution@proton.me>
2026-09-15 05:42:19 +0300

    u

--- home.nix
+++ home.nix
@@ -6,7 +6,7 @@
     stateVersion = "26.05";
     file.".config/mpv".source = ./.config/mpv;
     packages = [
-      (pkgs.writeShellScriptBin "xkeen-dscp" (builtins.readFile ./scripts/xkeen-run))
+      (pkgs.writeShellScriptBin "xkeen-run" (builtins.readFile ./scripts/xkeen-run))
       (pkgs.writeShellScriptBin "cs" (builtins.readFile ./scripts/cs))
     ];
     activation.xkeenVesktopDesktop = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
