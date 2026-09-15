1add76e7a63b5a032b2cbb0e361f1deb200b56ef
zxc-rv <zxc.revolution@proton.me>
2026-09-15 05:27:37 +0300

    u

--- home.nix
+++ home.nix
@@ -5,8 +5,8 @@
     homeDirectory = "/home/rv";
     stateVersion = "26.05";
     file.".config/mpv".source = ./.config/mpv;
-    packages = [ 
-      pkgs.writeShellScriptBin "cs" (builtins.readfile ./scripts/cs))
+    packages = [
+      (pkgs.writeShellScriptBin "cs" (builtins.readfile ./scripts/cs))
     ];
   };
   programs = {
